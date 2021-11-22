f69d33abe317cd55c5a856bb9161956ecc0a318b
QMIQIUQ <dogaang@gmail.com>
2021-11-22 09:16:35 +0800

    edit category controller

--- cart2021C/app/Http/Controllers/CategoryController.php
+++ cart2021C/app/Http/Controllers/CategoryController.php
@@ -12,11 +12,8 @@ class CategoryController extends Controller
     public function add(){
         $r=request();
         $addCategory=Category::create([
-            'name'=>$r->productName,
-            'image'=>$r->productImage,
-            'quantity'=>$r->productQuantity,
-            'price'=>$r->productPrice,
-            'discription'=>$r->productDiscription,
+            'name'=>$r->categoryName,
+
         ]);
         return view('addCategory');
     }
