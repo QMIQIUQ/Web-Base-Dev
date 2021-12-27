8f98413c7e6e7222b648f351825ec23733d16d17
QMIQIUQ <dogaang@gmail.com>
2021-12-27 09:59:29 +0800

    payment order id update

--- cart2021C/app/Http/Controllers/PaymentController.php
+++ cart2021C/app/Http/Controllers/PaymentController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Stripe;
+use DB;
+use Auth;
+use App\Models\myOrder;
+use App\Models\myCart;
+use Session;
+
+
 class PaymentController extends Controller
 {
     
@@ -18,8 +25,32 @@ class PaymentController extends Controller
                 "source" => $request->stripeToken,
                 "description" => "This payment is testing purpose of southern online",
         ]);
-           
+
+        //create record after paymnet
+        $newOrder=myOrder::Create([
+            'paymentStatus'=>'Done',
+            'userID'=>Auth::id(),
+            'amount' => $request->sub,
+        ]);
+
+        //get the order ID just now  created
+        $orderID=DB::table('my_orders')->where('userID','=',Auth::id())->orderBy('created_at','desc')->first();
+        $items=$request->input('cid');
+        
+
+        foreach($items as $item=>$value){
+            $carts=myCart::find($value);    //get the cart item record
+            $carts->orderID=$orderID->id;   //binding the order id with the record
+            $carts->save();
+        }
+        
         return back();
     }
+
+    public function view(){
+
+
+    }
+
 }
 
